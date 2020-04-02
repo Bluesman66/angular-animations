9e9d5e92a9c8178c855dc4046d391244773b983c
alex.paclin <alex.paclin@gmail.com>
2020-04-02 22:36:32 +0500

    5. Появление и исчезновение

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,12 +29,27 @@ import { Component } from "@angular/core";
 				style({ background: "green" }),
 				animate("1s", style({ background: "pink" })),
 				animate(750)
-			])
+      ]),
+      // void => *
+      transition(":enter", [
+        style({opacity: 0}),
+        animate("850ms ease-out")
+      ]),
+      // * => void
+      transition(":leave", [
+        style({opacity: 1}),
+        animate(750, style({
+          opacity: 0,
+          transform: "scale(1.2)"
+        }))
+      ])
+
 		])
 	]
 })
 export class AppComponent {
 	boxState = "end";
+	visible = true;
 
 	animate() {
 		this.boxState = this.boxState === "end" ? "start" : "end";
